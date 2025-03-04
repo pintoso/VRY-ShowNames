22d551cf3630ef0ebb5ca182d4a6500ee799047c
PINTOSO <otacopintoso@gmail.com>
2025-03-04 19:29:56 -0300

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -167,6 +167,7 @@ DEFAULT_CONFIG = {
         "table": {
             "skin": True,
             "rr": True,
+            "earned_rr": True,
             "peakrank": True,
             "previousrank" : True,
             "leaderboard": True,
